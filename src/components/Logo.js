9efa3347c37e772cc0fd88382ab3b47b14bd8d68
Alan <alanones02@gmail.com>
2023-04-17 09:06:30 +0300

    Initial commit

---
+++ src/components/Logo.js
@@ -0,0 +1,5 @@
+import logo from "../assets/images/logo.svg";
+const Logo = () => {
+  return <img src={logo} alt="jobs kenya logo" className="logo" />;
+};
+export default Logo;
---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import FormRow from "./FormRow";
+import Logo from "./Logo";
+import Navbar from "./Navbar";
+import BigSidebar from "./BigSidebar";
+import SmallSidebar from "./SmallSidebar";
+import FormRowSelect from "./FormRowSelect";
+import JobsContainer from "./JobsContainer";
+import SearchContainer from "./SearchContainer";
+import Loading from "./Loading";
+import StatsContainer from "./StatsContainer";
+import ChartsContainer from "./ChartsContainer";
+import BarChart from "./BarChart";
+import AreaChart from "./AreaChart";
+
+export {
+  Logo,
+  ChartsContainer,
+  FormRowSelect,
+  StatsContainer,
+  JobsContainer,
+  Loading,
+  SearchContainer,
+  FormRow,
+  BigSidebar,
+  SmallSidebar,
+  Navbar,
+  BarChart,
+  AreaChart,
+};
